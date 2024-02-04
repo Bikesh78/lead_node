b4870a804cc0e5e6766d07d2865b5120c2cb9b15
bikesh <bikeshrai78@gmail.com>
2024-02-04 13:03:51 +0545

    Add user entity

---
+++ entity/user.ts
@@ -0,0 +1,20 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { IsEmail } from "class-validator";
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  @IsEmail()
+  username: string;
+
+  @Column()
+  password?: string;
+}
